59fe8fb9b4ea57d603f0c82a62d4404f4b385cd7
Jayantakumar <jayantakumar.m@lgads.tv>
2024-12-10 22:38:40 +0530

    Added Some extra plugins

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,7 +6,15 @@ local lspconfig = require "lspconfig"
 -- EXAMPLE
 local servers = { "html", "cssls", "gopls" }
 local nvlsp = require "nvchad.configs.lspconfig"
-
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false})
+  end
+})
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
